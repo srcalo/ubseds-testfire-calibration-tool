a0691b546e01301d92b1d977622c4ff76054bd7c
djtring <70723709+djtring@users.noreply.github.com>
2022-08-25 01:45:49 -0400

    Used pandas for GetData

--- GetData.py
+++ GetData.py
@@ -1,6 +1,12 @@
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
+import pandas as pd
 
-filename = 'raw\\burn_raw.csv'
+filename = 'raw\\5-07-22_Test_Fire_Data_Raw.csv'
 
+df = pd.read_csv(filename,names=["values"])
+print(df)
+plt.plot(df)
+plt.ylabel("Voltage (mV)")
+plt.show()
