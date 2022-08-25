1caf853f90e768be134155b36e5144db8d036660
djtring <70723709+djtring@users.noreply.github.com>
2022-08-25 14:48:59 -0400

    Sucessfully filtered test fire data

--- GetData.py
+++ GetData.py
@@ -5,8 +5,14 @@ import pandas as pd
 
 filename = 'raw\\5-07-22_Test_Fire_Data_Raw.csv'
 
-df = pd.read_csv(filename,names=["values"])
-print(df)
-plt.plot(df)
+df = pd.read_csv(filename,names=['values'])
+staticValues = df['values'][0:10]
+maxVal = (np.max(staticValues))
+data = df[df['values'] > maxVal]
+data = data.reset_index(drop = True)
+data.to_csv("cutData.csv")
+
+print(data)
+plt.plot(data)
 plt.ylabel("Voltage (mV)")
 plt.show()
