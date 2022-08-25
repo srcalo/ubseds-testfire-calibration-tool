c5f41c38869ad471fd33773de17ea7d24d6bb11e
srcalo <smilingcalo@gmail.com>
2022-08-25 01:28:56 -0400

    started writing calibrator

--- calibrator.py
+++ calibrator.py
@@ -1,15 +1,36 @@
 import csv
 import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
 
-filename = 'data\calibration-data-raw.csv'
+# Config
+filename = 'data\\calibration-data-raw.csv'
+timeThresh = 0  # The minimum amount of time that a plateau exists before its counted
+maxVariance = 0 # Maximum difference between points before a plataeu no longer exists
+
 
 with open(filename) as File:
     data = csv.reader(File)
-    y = []
+    graph = []
     for row in data:
-        y.append(float(row[0]))
+        graph.append(float(row[0]))
 
-plt.plot(y)
+graphDiff = np.diff(graph)
+
+plt.plot(graphDiff)
+plt.ylabel("Voltage (mV)")
+plt.show()
+
+'''
+    Finds the values at which the graph plateaus by checking if 
+    a certain number of points on the graph remains nearly constant
+    for long enough
+'''
+for x, y in enumerate(graphDiff[:-1]):
+    x = 0
+    
+
+
+
+plt.plot(graphDiff)
 plt.ylabel("Voltage (mV)")
 plt.show()
