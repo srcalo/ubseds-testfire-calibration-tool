affbd0ee2b6fec718058771d2449d3ea23a0e425
djtring <70723709+djtring@users.noreply.github.com>
2022-08-25 23:50:16 -0400

    Cleaned up GetData

--- GetData.py
+++ GetData.py
@@ -2,62 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import time
+
 def cutData(dataset):
+    '''This function takes in a pandas dataset and returns
+    the trimmed data with some padding'''
 
-    t1 = time.time()
+    staticValues = dataset['values'][0:10] # takes the first ten data points
+    maxVal = (np.max(staticValues)) # gets the maximum of those values
+    booleanData = [dataset['values'] > maxVal] # creates an array of booleans that are True if they are > maxVal
 
-    staticValues = df['values'][0:10]
-    maxVal = (np.max(staticValues))
-    booleanData = [df['values'] > maxVal]
-    print(type(booleanData))
-
-
-    i = 0
-    indexFirstPoint = 0
-    indexLastPoint = 0
-    for i, bool in enumerate(booleanData[0]):
-        if bool:
-            indexFirstPoint = i
+    indexFirstPoint = 0 # index of the first True in booleanData
+    indexLastPoint = 0 # index of the last True in booleanData
+    for i, bool in enumerate(booleanData[0]): # iterates forwards through booleanData until it finds a true value then exits the loop
+        if bool: 
+            indexFirstPoint = i # saves the index of True value
             break
-
-    for i, bool in enumerate(reversed(booleanData[0])):
+    for i, bool in enumerate(reversed(booleanData[0])): # iterates backwards through booleanData until it finds a true value then exits the loop
         if bool:
-            indexLastPoint = np.size(booleanData) - i
+            indexLastPoint = np.size(booleanData) - i # saves the index of True value
             break
-
     
-    # while indexFirstPoint == 0:
-    #     if booleanData[0][i] == True:
-    #         indexFirstPoint = i
-    #     else:
-    #         i += 1
-    
-
-    # i = np.size(booleanData) - 1
-    # while indexLastPoint == 0:
-    #     if booleanData[0][i] == True:
-    #         indexLastPoint = i
-    #     else:
-    #         i -= 1
-    
-    cutData = df[indexFirstPoint-50:indexLastPoint+50]
-    cutData = cutData.reset_index(drop = True)
-
-    t2 = time.time()
-    print(t2 - t1)
+    cutData = dataset[indexFirstPoint-50:indexLastPoint+50] # creates the trimmed data with 50 points of padding on both sides
+    cutData = cutData.reset_index(drop = True) # resets the indexes after trimming
 
     return cutData
 
 filename = 'raw\\5-07-22_Test_Fire_Data_Raw.csv'
 
-df = pd.read_csv(filename,names=['values'])
+df = pd.read_csv(filename,names=['values']) # turns csv file into a usable dataset 
 
-data = cutData(df)
+data = cutData(df) # gets the cut data
 
-data.to_csv("cutData.csv")
+data.to_csv("cutData.csv") # creates a csv file with just the cut data
 
-
-print(data)
-plt.plot(data)
+# plots the cut data
+plt.plot(data) 
 plt.ylabel("Voltage (mV)")
 plt.show()
